4fded8dd92c0f5cb1ee7a3d4dc8895a43fb53e7e
root <root@HW0010602>
2023-04-24 17:02:15 +0800

    update file browser

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -60,11 +60,14 @@ M.nvimtree = {
 M.telescope = {
   extensions = {
     fzf = {
-      fuzzy = true,                   -- false will only do exact matching
+      fuzzy = true, -- false will only do exact matching
       override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true,    -- override the file sorter
-      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
+    },
+    file_browser = {
+      hidden = true,
     },
   },
   extensions_list = { "themes", "terms", "projects", "fzf", "file_browser" },
