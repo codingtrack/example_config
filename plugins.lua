7008f3d1993ebd4d0a959d6260b2eaac8da696c8
codingtrack <771521846@qq.com>
2023-04-25 20:46:39 +0800

    update wilder

--- plugins.lua
+++ plugins.lua
@@ -125,15 +125,36 @@ local plugins = {
     config = function()
       local wilder = require "wilder"
       wilder.setup { modes = { ":", "/", "?" } }
+
+      local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
+        border = "rounded",
+        empty_message = wilder.popupmenu_empty_message_with_spinner(),
+        highlighter = wilder.basic_highlighter(),
+        left = {
+          " ",
+          wilder.popupmenu_devicons(),
+          wilder.popupmenu_buffer_flags {
+            flags = " a + ",
+            icons = { ["+"] = "", a = "", h = "" },
+          },
+        },
+        right = {
+          " ",
+          wilder.popupmenu_scrollbar(),
+        },
+      })
+
+      local wildmenu_renderer = wilder.wildmenu_renderer {
+        highlighter = wilder.basic_highlighter(),
+        separator = " · ",
+      }
+
       wilder.set_option(
         "renderer",
         wilder.renderer_mux {
-          [":"] = wilder.popupmenu_renderer {
-            highlighter = wilder.basic_highlighter(),
-          },
-          ["/"] = wilder.wildmenu_renderer {
-            highlighter = wilder.basic_highlighter(),
-          },
+          [":"] = popupmenu_renderer,
+          ["/"] = wildmenu_renderer,
+          substitute = wildmenu_renderer,
         }
       )
     end,
