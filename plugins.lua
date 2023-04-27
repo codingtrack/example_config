a52a4a1cbc44dadd4dc1a61bbaf21b17233fd445
codingtrack <771521846@qq.com>
2023-04-27 23:30:26 +0800

    add bigfile

--- plugins.lua
+++ plugins.lua
@@ -77,7 +77,7 @@ local plugins = {
   {
     "TimUntersberger/neogit",
     cmd = { "Neogit" },
-    dependencies = { "sindrets/diffview.nvim", event = "BufReadPost" },
+    dependencies = { "sindrets/diffview.nvim", cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" } },
     config = function()
       require("neogit").setup {
         integrations = { diffview = true },
@@ -192,6 +192,42 @@ local plugins = {
       dofile(vim.g.base46_cache .. "notify")
       require("notify").setup()
       vim.notify = require "notify"
+    end,
+  },
+  {
+    "LunarVim/bigfile.nvim",
+    event = { "BufReadPost", "BufReadPre", "BufRead" },
+    config = function()
+      local ftdetect = {
+        name = "ftdetect",
+        opts = { defer = true },
+        disable = function()
+          vim.api.nvim_set_option_value("filetype", "big_file_disabled_ft", { scope = "local" })
+        end,
+      }
+
+      local cmp = {
+        name = "nvim-cmp",
+        opts = { defer = true },
+        disable = function()
+          require("cmp").setup.buffer { enabled = false }
+        end,
+      }
+
+      require("bigfile").config {
+        filesize = 1, -- size of the file in MiB
+        pattern = { "*" }, -- autocmd pattern
+        features = { -- features to disable
+          "indent_blankline",
+          "lsp",
+          "illuminate",
+          "treesitter",
+          "syntax",
+          "vimopts",
+          ftdetect,
+          cmp,
+        },
+      }
     end,
   },
 }
