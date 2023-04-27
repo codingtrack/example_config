a560506c40cdac24ffbf139559f5e35197125c7c
codingtrack <771521846@qq.com>
2023-04-27 23:48:11 +0800

    support paste keymap for neovide

--- init.lua
+++ init.lua
@@ -47,4 +47,9 @@ if vim.g.neovide then
   vim.g.neovide_fullscreen = false
   vim.g.neovide_input_use_logo = true -- true on macOS
   vim.opt.mousemoveevent = true
+  vim.keymap.set('v', '<C-S-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<C-S-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<C-S-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<C-S-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<C-S-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
